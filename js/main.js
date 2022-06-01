9a42c117a974ca510e17380da44b0be7431f9ed5
Tuba Sheikh <tuba.sheikh1@dcmail.ca>
2022-06-01 17:20:39 -0400

    added js for scroll

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
-const email = document.querySelector(".fa-envelope");
-email.addEventListener("click", function(){
-    window.open("https://www.google.com/", "blank");
+/* NAV LINKS */
+const techStack = document.querySelector(".nav_tech_stack");
+
+techStack.addEventListener("click", (event) => {
+    event.preventDefault();
+    document.querySelector("#languages").scrollIntoView({behavior: "smooth"});
+});
+
+const about = document.querySelector(".nav_about");
+
+techStack.addEventListener("click", (event) => {
+    event.preventDefault();
+    document.querySelector("#about").scrollIntoView({behavior: "smooth"});
+});
+
+
+const contact = document.querySelector(".nav_contact");
+
+techStack.addEventListener("click", (event) => {
+    event.preventDefault();
+    document.querySelector("#contact").scrollIntoView({behavior: "smooth"});
+});
+
+const projects = document.querySelector(".nav_projects");
+
+techStack.addEventListener("click", (event) => {
+    event.preventDefault();
+    document.querySelector("#projects").scrollIntoView({behavior: "smooth"});
 });
