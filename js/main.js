e6aa7c251d10c466272505a10b9c1e5a712cb994
Tuba Sheikh <tuba.sheikh1@dcmail.ca>
2022-06-02 22:50:28 -0400

    added icon and fixed font

--- js/main.js
+++ js/main.js
@@ -1,29 +1,27 @@
 /* NAV LINKS */
 const techStack = document.querySelector(".nav_tech_stack");
+const about = document.querySelector(".nav_about");
+const contact = document.querySelector(".nav_contact");
+const projects = document.querySelector(".nav_projects");
 
 techStack.addEventListener("click", (event) => {
     event.preventDefault();
     document.querySelector("#languages").scrollIntoView({behavior: "smooth"});
 });
 
-const about = document.querySelector(".nav_about");
-
 techStack.addEventListener("click", (event) => {
     event.preventDefault();
     document.querySelector("#about").scrollIntoView({behavior: "smooth"});
 });
-
-
-const contact = document.querySelector(".nav_contact");
 
 techStack.addEventListener("click", (event) => {
     event.preventDefault();
     document.querySelector("#contact").scrollIntoView({behavior: "smooth"});
 });
 
-const projects = document.querySelector(".nav_projects");
-
 techStack.addEventListener("click", (event) => {
     event.preventDefault();
     document.querySelector("#projects").scrollIntoView({behavior: "smooth"});
 });
+
+
